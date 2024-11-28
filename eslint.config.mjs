177a3b7ecc9db8a4828fd1d824adafe2dce3817e
Teages <Teages@teages.xyz>
2024-11-29 01:51:14 +0800

    chore: update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,9 +3,10 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   rules: {
-    'curly': ['error', 'all'],
-    'no-console': 'off',
+    'curly': ['error', 'all'], // my personal preference
+    'no-console': 'off', // playground needs console.log
   },
+  // some other options
   // stylistic: {
   //   indent: 4,
   //   quotes: 'double',
